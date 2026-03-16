8148fcc052271206ee6b266bb0a7438aadd53328
CL <cyanraindrop.outlook.com>
2026-03-16 16:30:35 +0800

    'refine_logo'

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="login-container">
     <el-card class="login-card">
-      <h2>Admin Login</h2>
+      <h2>管理员登录</h2>
       <el-form :model="form" label-width="80px">
-        <el-form-item label="Username">
+        <el-form-item label="用户名">
           <el-input v-model="form.username"></el-input>
         </el-form-item>
-        <el-form-item label="Password">
+        <el-form-item label="密码">
           <el-input v-model="form.password" type="password"></el-input>
         </el-form-item>
         <el-form-item>
